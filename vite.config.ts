5f43589faade786cf587a433cf268ab322f0a4cd
mty <matianyue6@gmail.com>
2025-08-22 17:25:08 +0100

    Restore complete project from backup

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mine-ecology-center/',
+  base: process.env.NODE_ENV === 'production' ? '/mine-ecology-center/' : '/',
 })
